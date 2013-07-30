75228aa5e0511a3b36dafa15faf8fb86256be5ed
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2013-07-30 14:29:27 +0300

    transcode: Add result processing script

---
+++ transcode/results/compute-avg-min-max.R
@@ -0,0 +1,17 @@
+args <- commandArgs(trailingOnly=TRUE)
+source_file <- args[1]
+
+# Read table from input file and group by executable name.
+
+d <- read.table(source_file, header=T, sep=",")
+
+cm = d$cachemisses
+rt = d$runningtime
+
+# Print cache misses values and time to run values for each group.
+cat("executable", "type", "mean", "min", "max", "sd", "rsd", sep=","); cat("\n")
+
+cat("mem-walk-no-thread-no-thrash", "cache-misses", mean(cm), min(cm), max(cm), sd(cm), 100*sd(cm)/mean(cm), sep=","); cat("\n")
+cat("mem-walk-no-thread-no-thrash", "running-time", mean(rt), min(rt), max(rt), sd(rt), 100*sd(rt)/mean(rt), sep=","); cat("\n")
+
+q()
