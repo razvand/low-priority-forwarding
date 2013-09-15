dedc0baafd071aa3c17b5cbf221fad6bbda82ead
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2013-09-15 15:41:36 +0300

    transcode: Use proper executable name in compute-avg-min-max script

--- transcode/results/compute-avg-min-max.R
+++ transcode/results/compute-avg-min-max.R
@@ -11,7 +11,7 @@ rt = d$runningtime
 # Print cache misses values and time to run values for each group.
 cat("executable", "type", "mean", "min", "max", "sd", "rsd", sep=","); cat("\n")
 
-cat("mem-walk-no-thread-no-thrash", "cache-misses", mean(cm), min(cm), max(cm), sd(cm), 100*sd(cm)/mean(cm), sep=","); cat("\n")
-cat("mem-walk-no-thread-no-thrash", "running-time", mean(rt), min(rt), max(rt), sd(rt), 100*sd(rt)/mean(rt), sep=","); cat("\n")
+cat("transcode", "cache-misses", mean(cm), min(cm), max(cm), sd(cm), 100*sd(cm)/mean(cm), sep=","); cat("\n")
+cat("transcode", "running-time", mean(rt), min(rt), max(rt), sd(rt), 100*sd(rt)/mean(rt), sep=","); cat("\n")
 
 q()
